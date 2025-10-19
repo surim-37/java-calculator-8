c924cb097d2a9d6fd9e2a9ada12aff269d0f1cbb
Lee Sruim <sssurim@naver.com>
2025-10-20 05:54:32 +0900

    입/출력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+<<<<<<< HEAD
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
 }
+=======
+import camp.nextstep.edu.missionutils.Console;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        int result = StringAdd.calculate(input);  // 클래스명 변경됨
+        System.out.println("결과 : " + result);
+    }
+}
+
+>>>>>>> f52a522 (입/출력 기능 구현)
